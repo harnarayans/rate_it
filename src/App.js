29159b6673d16471910b9eb8ba19fb6fbb53f5fa
Singh <har.narayan.singh@sap.com>
2021-03-03 00:48:52 +0530

    material list from JSON array

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Reviews from './mock_data/reviews.json';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <List>
+      {Reviews.map(item => (
+                <ListItem><ListItemText primary={item.review_summary} secondary={item.posted_by} /></ListItem>
+              ))}
+      </List>
     </div>
   );
 }
